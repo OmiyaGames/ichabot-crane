cb8b9810e56438fd68ed4fb682d2d4f4ed9f8f10
Japtar10101@gmail.com <Japtar10101@gmail.com@1aea3f52-b791-e006-8dda-67420ba60bea>
2014-01-25 10:23:38 +0000

    Starting on adding server and client.

---
+++ trunk/Assets/Scripts/SetupServer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetupServer : MonoBehaviour {
+	public const int Port = 25000;
+	public const string Password = "Testing...";
+
+	// Use this for initialization
+	void Start () {
+		Network.maxConnections = 4;
+		Network.incomingPassword = Password;
+		Network.InitializeServer(32, Port);
+	}
+}
