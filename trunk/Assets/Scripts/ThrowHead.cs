b5260001b862fa1546650a7339f7896b66fe86ea
Japtar10101@gmail.com <Japtar10101@gmail.com@1aea3f52-b791-e006-8dda-67420ba60bea>
2014-01-25 03:05:45 +0000

    Got the head pickup working.

--- trunk/Assets/Scripts/ThrowHead.cs
+++ trunk/Assets/Scripts/ThrowHead.cs
@@ -3,25 +3,61 @@ using System.Collections;
 
 public class ThrowHead : MonoBehaviour
 {
-	public GameObject permanentlyAttachedCamera;
-	public Rigidbody temporarilyAttachedCamera;
-	public float throwForce = 1000;
+	[SerializeField] GameObject permanentlyAttachedCamera;
+	[SerializeField] Rigidbody temporarilyAttachedCamera;
+	[SerializeField] float throwForce = 30;
+	[SerializeField] float distanceFromBodyBeforePickingUp = 2;
+	[SerializeField] GUIText pickupText;
+
 	bool isHeadAttached = true;
 
-	// Use this for initialization
-	void Start ()
+	public bool IsHeadAttached
 	{
+		get
+		{
+			return isHeadAttached;
+		}
 	}
-	
+
+	void Start()
+	{
+		pickupText.enabled = false;
+	}
+
 	// Update is called once per frame
 	void Update ()
 	{
-		if((isHeadAttached == true) && (Input.GetAxis("Fire1").CompareTo(0) != 0))
+		if(isHeadAttached == true)
 		{
-			permanentlyAttachedCamera.SetActive(false);
-			temporarilyAttachedCamera.gameObject.SetActive(true);
-			temporarilyAttachedCamera.transform.parent = null;
-			temporarilyAttachedCamera.AddForce(permanentlyAttachedCamera.transform.forward * throwForce, ForceMode.VelocityChange);
+			if(Input.GetMouseButtonDown(0) == true)
+			{
+				permanentlyAttachedCamera.SetActive(false);
+				temporarilyAttachedCamera.gameObject.SetActive(true);
+				temporarilyAttachedCamera.transform.parent = null;
+				temporarilyAttachedCamera.AddForce(permanentlyAttachedCamera.transform.forward * throwForce, ForceMode.VelocityChange);
+				isHeadAttached = false;
+			}
+		}
+		else if(Vector3.Distance(permanentlyAttachedCamera.transform.position, temporarilyAttachedCamera.transform.position) < distanceFromBodyBeforePickingUp)
+		{
+			if(Input.GetMouseButtonDown(0) == true)
+			{
+				permanentlyAttachedCamera.SetActive(true);
+				temporarilyAttachedCamera.gameObject.SetActive(false);
+				temporarilyAttachedCamera.transform.parent = permanentlyAttachedCamera.transform;
+				temporarilyAttachedCamera.transform.localPosition = Vector3.zero;
+				temporarilyAttachedCamera.transform.localRotation = Quaternion.identity;
+				pickupText.enabled = false;
+				isHeadAttached = true;
+			}
+			else
+			{
+				pickupText.enabled = true;
+			}
+		}
+		else
+		{
+			pickupText.enabled = false;
 		}
 	}
 }
