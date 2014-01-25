489e31c79fc5b687fc3e7cb1d4dd51cfd9c0c223
Japtar10101@gmail.com <Japtar10101@gmail.com@1aea3f52-b791-e006-8dda-67420ba60bea>
2014-01-25 03:48:52 +0000

    Attempting to add head rolling controls.

--- Assets/Scripts/RollHead.cs
+++ Assets/Scripts/RollHead.cs
@@ -1,15 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody))]
 public class RollHead : MonoBehaviour {
+	// A mouselook behaviour with constraints which operate relative to
+	// this gameobject's initial rotation.
+	
+	// Only rotates around local X and Y.
+	
+	// Works in local coordinates, so if this object is parented
+	// to another moving gameobject, its local constraints will
+	// operate correctly
+	// (Think: looking out the side window of a car, or a gun turret
+	// on a moving spaceship with a limited angular range)
+	
+	// to have no constraints on an axis, set the rotationRange to 360 or greater.
+	
+	public float rotationSpeed = 10;
+	public float dampingTime = 0.2f;
+	public bool autoZeroVerticalOnMobile = true;
+	public bool autoZeroHorizontalOnMobile = false;
+	Vector3 targetAngles;
+	Vector3 followAngles;
+	Vector3 followVelocity;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+
+		// read input from mouse or mobile controls
+		float inputH = CrossPlatformInput.GetAxis("Mouse X");
+		float inputV = CrossPlatformInput.GetAxis("Mouse Y");
+
+		#if UNITY_IPHONE || UNITY_ANDROID || UNITY_WP8
+		// on mobile, sometimes we want input mapped directly to tilt value,
+		// so it springs back automatically when the look input is released.
+		if (autoZeroHorizontalOnMobile) {
+			targetAngles.y = Mathf.Lerp (-rotationRange.y * 0.5f, rotationRange.y * 0.5f, inputH * .5f + .5f);
+		} else {
+			targetAngles.y += inputH * rotationSpeed;
+		}
+		if (autoZeroVerticalOnMobile) {
+			targetAngles.x = Mathf.Lerp (-rotationRange.x * 0.5f, rotationRange.x * 0.5f, inputV * .5f + .5f);
+		} else {
+			targetAngles.x += inputV * rotationSpeed;
+		}
+		#else
+		// with mouse input, we have direct control with no springback required.
+		targetAngles.y = inputH * rotationSpeed;
+		targetAngles.x = inputV * -rotationSpeed;
+		#endif
+		
+		// smoothly interpolate current values to target angles
+		//followAngles = Vector3.SmoothDamp( followAngles, targetAngles, ref followVelocity, dampingTime );
+		
+		// update the actual gameobject's rotation
+		rigidbody.AddRelativeTorque(targetAngles, ForceMode.VelocityChange);
 	}
 }
--- Assets/Scripts/ThrowHead.cs
+++ Assets/Scripts/ThrowHead.cs
@@ -7,9 +7,11 @@ public class ThrowHead : MonoBehaviour
 	[SerializeField] Rigidbody temporarilyAttachedCamera;
 	[SerializeField] float throwForce = 30;
 	[SerializeField] float distanceFromBodyBeforePickingUp = 2;
+	[SerializeField] SimpleMouseRotator[] allMouseRotationScripts;
 	[SerializeField] GUIText pickupText;
 
 	bool isHeadAttached = true;
+	bool isFireButtonDown = false;
 
 	public bool IsHeadAttached
 	{
@@ -27,26 +29,50 @@ public class ThrowHead : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+		// Check to see if fire has been clicked
+		bool fireButtonPressed = false;
+		if(Input.GetAxis("Fire1").CompareTo(0) != 0)
+		{
+			if(isFireButtonDown == false)
+			{
+				fireButtonPressed = true;
+				isFireButtonDown = true;
+			}
+		}
+		else
+		{
+			isFireButtonDown = false;
+		}
+
+		// Check if head is attached
 		if(isHeadAttached == true)
 		{
-			if(Input.GetMouseButtonDown(0) == true)
+			if(fireButtonPressed == true)
 			{
 				permanentlyAttachedCamera.SetActive(false);
 				temporarilyAttachedCamera.gameObject.SetActive(true);
 				temporarilyAttachedCamera.transform.parent = null;
 				temporarilyAttachedCamera.AddForce(permanentlyAttachedCamera.transform.forward * throwForce, ForceMode.VelocityChange);
+				foreach(SimpleMouseRotator rotator in allMouseRotationScripts)
+				{
+					rotator.enabled = false;
+				}
 				isHeadAttached = false;
 			}
 		}
 		else if(Vector3.Distance(permanentlyAttachedCamera.transform.position, temporarilyAttachedCamera.transform.position) < distanceFromBodyBeforePickingUp)
 		{
-			if(Input.GetMouseButtonDown(0) == true)
+			if(fireButtonPressed == true)
 			{
 				permanentlyAttachedCamera.SetActive(true);
 				temporarilyAttachedCamera.gameObject.SetActive(false);
 				temporarilyAttachedCamera.transform.parent = permanentlyAttachedCamera.transform;
 				temporarilyAttachedCamera.transform.localPosition = Vector3.zero;
 				temporarilyAttachedCamera.transform.localRotation = Quaternion.identity;
+				foreach(SimpleMouseRotator rotator in allMouseRotationScripts)
+				{
+					rotator.enabled = false;
+				}
 				pickupText.enabled = false;
 				isHeadAttached = true;
 			}
