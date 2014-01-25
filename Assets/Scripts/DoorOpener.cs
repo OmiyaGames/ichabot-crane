3def5c320efb8ae3223e745a118843029e1cdee4
Japtar10101@gmail.com <Japtar10101@gmail.com@1aea3f52-b791-e006-8dda-67420ba60bea>
2014-01-25 15:36:22 +0000

    Adding switch and door system.

---
+++ Assets/Scripts/DoorOpener.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpener : MonoBehaviour
+{
+	public float moveUpBy = 5;
+	public Switch activateSwitch;
+	public bool holdToOpen = false;
+	public float smoothFactor = 1;
+
+	bool isOpen = false;
+	Vector3 originalPosition;
+	Vector3 openPosition;
+	Vector3 velocity;
+
+	// Use this for initialization
+	void Start ()
+	{
+		activateSwitch.OnSwitchEnter += SwitchEnter;
+		activateSwitch.OnSwitchExit += SwitchExit;
+		originalPosition = transform.position;
+		openPosition = originalPosition;
+		openPosition += transform.up * moveUpBy;
+	}
+
+	void SwitchEnter(bool isHead)
+	{
+		isOpen = true;
+	}
+
+	void SwitchExit(bool isHead)
+	{
+		if(holdToOpen == true)
+		{
+			isOpen = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 targetPosition = originalPosition;
+		if(isOpen == true)
+		{
+			targetPosition = openPosition;
+		}
+
+		transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, Time.deltaTime * smoothFactor);
+	}
+}
---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class Switch : MonoBehaviour {
+	public bool triggerOnBody = true;
+	public bool triggerOnHead = true;
+	public event System.Action<bool> OnSwitchEnter;
+	public event System.Action<bool> OnSwitchExit;
+
+	void Start()
+	{
+		collider.isTrigger = true;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if((enabled == true) && (OnSwitchEnter != null) && (other != null))
+		{
+			if((other.CompareTag("Player") == true) && (triggerOnBody == true))
+			{
+				OnSwitchEnter(false);
+			}
+			else if((other.CompareTag("Player1") == true) && (triggerOnHead == true))
+			{
+				OnSwitchEnter(true);
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if((enabled == true) && (OnSwitchExit != null) && (other != null))
+		{
+			if((other.CompareTag("Player") == true) && (triggerOnBody == true))
+			{
+				OnSwitchExit(false);
+			}
+			else if((other.CompareTag("Player1") == true) && (triggerOnHead == true))
+			{
+				OnSwitchExit(true);
+			}
+		}
+	}
+}
