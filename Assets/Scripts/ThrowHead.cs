a75c789b1742fd5af68bb7757971faa724eaada2
Japtar10101@gmail.com <Japtar10101@gmail.com@1aea3f52-b791-e006-8dda-67420ba60bea>
2014-01-25 02:30:45 +0000

    Putting together the project.

---
+++ Assets/Scripts/ThrowHead.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowHead : MonoBehaviour
+{
+	public GameObject permanentlyAttachedCamera;
+	public Rigidbody temporarilyAttachedCamera;
+	public float throwForce = 1000;
+	bool isHeadAttached = true;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if((isHeadAttached == true) && (Input.GetAxis("Fire1").CompareTo(0) != 0))
+		{
+			permanentlyAttachedCamera.SetActive(false);
+			temporarilyAttachedCamera.gameObject.SetActive(true);
+			temporarilyAttachedCamera.transform.parent = null;
+			temporarilyAttachedCamera.AddForce(permanentlyAttachedCamera.transform.forward * throwForce, ForceMode.VelocityChange);
+		}
+	}
+}
