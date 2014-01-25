0a0a9fb02da2b84e70981fa96e5fcbd2e8278d9f
Japtar10101@gmail.com <Japtar10101@gmail.com@1aea3f52-b791-e006-8dda-67420ba60bea>
2014-01-25 10:54:54 +0000

    Learning how client-server connection worked.

--- trunk/Assets/Scripts/SetupClient.cs
+++ trunk/Assets/Scripts/SetupClient.cs
@@ -1,15 +1,95 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SetupClient : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
+public class SetupClient : MonoBehaviour
+{
+	enum State
+	{
+		AwaitingInput,
+		Connecting,
+		Connected
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	static SetupClient instance = null;
+	State currentState = State.AwaitingInput;
+	Rect fullRect;
+	Rect tempRect;
+	string ipAddress = string.Empty;
+
+	void Start()
+	{
+		if(instance == null)
+		{
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			GameObject.Destroy(gameObject);
+		}
+	}
+
+	void OnGUI()
+	{
+		fullRect.width = Screen.width / 2;
+		fullRect.height = Screen.height / 2;
+		fullRect.center = new Vector2(Screen.width / 2, Screen.height / 2);
+
+		switch(currentState)
+		{
+		case State.AwaitingInput:
+			tempRect = fullRect;
+			tempRect.height /= 3;
+			GUI.Label(tempRect, "Enter server address");
+
+			tempRect.y += tempRect.height;
+			ipAddress = GUI.TextField(tempRect, ipAddress);
+
+			tempRect.y += tempRect.height;
+			if(GUI.Button(tempRect, "Connect") == true)
+			{
+				Network.Connect(ipAddress, SetupServer.Port, SetupServer.Password);
+				currentState = State.Connecting;
+			}
+			break;
+		case State.Connecting:
+			tempRect = fullRect;
+			GUI.Label(tempRect, "Connecting...");
+			break;
+		case State.Connected:
+			tempRect = fullRect;
+			tempRect.height /= 3;
+			GUI.Label(tempRect, "Connected to:");
+			
+			tempRect.y += tempRect.height;
+			GUI.Label(tempRect, ipAddress);
+
+			tempRect.y += tempRect.height;
+			if(GUI.Button(tempRect, "Disconnect") == true)
+			{
+				Network.Disconnect();
+			}
+			break;
+		}
+	}
+
+	void OnConnectedToServer()
+	{
+		currentState = State.Connected;
+	}
+
+	void OnFailedToConnect(NetworkConnectionError error)
+	{
+		currentState = State.AwaitingInput;
+	}
+
+	void OnDisconnectedFromServer(NetworkDisconnection reason)
+	{
+		currentState = State.AwaitingInput;
+	}
+
+	void OnApplicationQuit()
+	{
+		Network.Disconnect();
 	}
 }
--- trunk/Assets/Scripts/SetupServer.cs
+++ trunk/Assets/Scripts/SetupServer.cs
@@ -4,11 +4,32 @@ using System.Collections;
 public class SetupServer : MonoBehaviour {
 	public const int Port = 25000;
 	public const string Password = "Testing...";
+	Rect fullRect;
+	System.Text.StringBuilder labelText = new System.Text.StringBuilder();
 
 	// Use this for initialization
 	void Start () {
 		Network.maxConnections = 4;
 		Network.incomingPassword = Password;
 		Network.InitializeServer(32, Port);
+	}
+
+	void OnPlayerConnected(NetworkPlayer player)
+	{
+		labelText.AppendLine("Connected player " + player.ipAddress);
+	}
+
+	void OnPlayerDisconnected(NetworkPlayer player)
+	{
+		labelText.AppendLine("Disconnected player " + player.ipAddress);
+	}
+
+	void OnGUI()
+	{
+		fullRect.width = Screen.width / 2;
+		fullRect.height = Screen.height / 2;
+		fullRect.center = new Vector2(Screen.width / 2, Screen.height / 2);
+		
+		GUI.Label(fullRect, labelText.ToString());
 	}
 }
