71a683d23ad45bb98d11928936e73f6f0f771b21
Japtar10101@gmail.com <Japtar10101@gmail.com@1aea3f52-b791-e006-8dda-67420ba60bea>
2014-01-26 05:15:13 +0000

    Adding an animation to move the head onto the robot.

--- trunk/Assets/Scripts/ThrowHead.cs
+++ trunk/Assets/Scripts/ThrowHead.cs
@@ -1,18 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(FirstPersonCharacter))]
 public class ThrowHead : MonoBehaviour
 {
+	public const float AnimationSnapFactor = 0.1f;
 	[SerializeField] GameObject permanentlyAttachedCamera;
 	[SerializeField] SimpleMouseRotator temporarilyAttachedCamera;
 	[SerializeField] HopHead headHopper;
 	[SerializeField] float throwForce = 30;
 	[SerializeField] float distanceFromBodyBeforePickingUp = 2;
 	[SerializeField] bool enableHeadTossing = true;
+	[SerializeField] float headAttachementLerpFactor = 1;
+	[SerializeField] float headRotationLerpFactor = 1;
 	SimpleMouseRotator[] allMouseRotationScripts;
-
+	FirstPersonCharacter controller;
 	bool isHeadAttached = true;
 	bool isFireButtonDown = false;
+	bool isAnimatingHeadAttachement = false;
 	static bool isPaused = false;
 
 	public bool IsHeadAttached
@@ -46,7 +51,9 @@ public class ThrowHead : MonoBehaviour
 	void Start()
 	{
 		allMouseRotationScripts = GetComponentsInChildren<SimpleMouseRotator>();
+		controller = GetComponent<FirstPersonCharacter>();
 		isPaused = false;
+		isAnimatingHeadAttachement = false;
 		Screen.lockCursor = true;
 	}
 
@@ -78,11 +85,35 @@ public class ThrowHead : MonoBehaviour
 		// Check if head is attached
 		if(isHeadAttached == true)
 		{
-			if((enableHeadTossing == true) && (fireButtonPressed == true))
+			if(isAnimatingHeadAttachement == true)
+			{
+				if(Vector3.Distance(temporarilyAttachedCamera.transform.position, permanentlyAttachedCamera.transform.position) < AnimationSnapFactor)
+				{
+					controller.isControlsEnabled = true;
+					isAnimatingHeadAttachement = false;
+					temporarilyAttachedCamera.transform.parent = permanentlyAttachedCamera.transform;
+					permanentlyAttachedCamera.SetActive(true);
+					temporarilyAttachedCamera.gameObject.SetActive(false);
+					temporarilyAttachedCamera.transform.localPosition = Vector3.zero;
+					temporarilyAttachedCamera.transform.localRotation = Quaternion.identity;
+					foreach(SimpleMouseRotator rotator in allMouseRotationScripts)
+					{
+						rotator.enabled = true;
+					}
+				}
+				else
+				{
+					temporarilyAttachedCamera.transform.position = Vector3.Lerp(temporarilyAttachedCamera.transform.position, permanentlyAttachedCamera.transform.position, Time.deltaTime * headAttachementLerpFactor);
+					temporarilyAttachedCamera.transform.rotation = Quaternion.Lerp(temporarilyAttachedCamera.transform.rotation, permanentlyAttachedCamera.transform.rotation, Time.deltaTime * headRotationLerpFactor);
+					headHopper.cameraControl.transform.rotation = Quaternion.Lerp(headHopper.cameraControl.transform.rotation, permanentlyAttachedCamera.transform.rotation, Time.deltaTime * headRotationLerpFactor);
+				}
+			}
+			else if((enableHeadTossing == true) && (fireButtonPressed == true))
 			{
 				permanentlyAttachedCamera.SetActive(false);
 				temporarilyAttachedCamera.gameObject.SetActive(true);
 				temporarilyAttachedCamera.transform.parent = null;
+				temporarilyAttachedCamera.rigidbody.isKinematic = false;
 				temporarilyAttachedCamera.rigidbody.velocity = rigidbody.velocity;
 				temporarilyAttachedCamera.rigidbody.AddForce(permanentlyAttachedCamera.transform.forward * throwForce, ForceMode.VelocityChange);
 				temporarilyAttachedCamera.Start();
@@ -99,17 +130,12 @@ public class ThrowHead : MonoBehaviour
 			PauseMenu.ShowMessage("Click to pick up head");
 			if(fireButtonPressed == true)
 			{
-				permanentlyAttachedCamera.SetActive(true);
-				temporarilyAttachedCamera.gameObject.SetActive(false);
-				temporarilyAttachedCamera.transform.parent = permanentlyAttachedCamera.transform;
-				temporarilyAttachedCamera.transform.localPosition = Vector3.zero;
-				temporarilyAttachedCamera.transform.localRotation = Quaternion.identity;
-				foreach(SimpleMouseRotator rotator in allMouseRotationScripts)
-				{
-					rotator.enabled = true;
-				}
+				temporarilyAttachedCamera.rigidbody.isKinematic = true;
 				isHeadAttached = true;
+				isAnimatingHeadAttachement = true;
+				headHopper.ReattachHead();
 				PauseMenu.HideMessage();
+				controller.isControlsEnabled = false;
 			}
 		}
 		else
